620e43a8b4a8609ceb59d6b76627c19f1609cece
Diego Zahid Quispe Quispe <diego.quispe.qui@tecsup.edu.pe>
2025-06-10 14:53:51 -0500

    Cambios en MapScreen
    + se añadió el icono en res/drawable
    + tambien se añadio una funcion para redimensionar el icono para maps
    + este ultimo se añadio dentro de Marker

--- app/src/main/java/com/quispe/lab12/MapScreen.kt
+++ app/src/main/java/com/quispe/lab12/MapScreen.kt
@@ -1,16 +1,31 @@
 package com.quispe.lab12
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
+
+@Composable
+fun rememberSmallMountainIcon(): BitmapDescriptor {
+    val context = LocalContext.current
+    return remember {
+        val originalBitmap = BitmapFactory.decodeResource(context.resources, R.drawable._1mountain)
+        val scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, 80, 80, false)
+        BitmapDescriptorFactory.fromBitmap(scaledBitmap)
+    }
+}
 
 
 @Composable
@@ -27,11 +42,14 @@ fun MapScreen() {
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
+            val mountainIcon = rememberSmallMountainIcon()
+            // Añadir marcador en Arequipa Perú
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
+                icon = mountainIcon,
                 title = "Arequipa, Perú"
             )
+
         }
     }
 }
